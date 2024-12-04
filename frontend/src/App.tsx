29bbf5047b949df2b82f8b9e13c201c6f7a5674b
Vstahelin <vitorjosestahelin@gmail.com>
2024-12-04 16:20:38 -0300

    fix: add NotFound page and update routing for production environment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path={
+            process.env.NODE_ENV === 'production' ? '/calendario-istepo/' : '/'
+          }
+          element={<Home />}
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
