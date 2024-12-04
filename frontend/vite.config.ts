c55ccd35a5937ba090cf6296572d841cb9cb802e
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2024-12-04 00:42:43 -0300

    fix: update dependencies in package.json and package-lock.json, refactor vite.config.ts for improved build configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,53 +3,71 @@ import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'development' ? '/' : '/calendario-istepo/',
-    plugins: [
-      react(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: {
-          enabled: true,
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+      includeAssets: ['vite.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'React Template',
+        short_name: 'App',
+        description: 'React Template with Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  build: {
+    manifest: true,
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendors';
+            }
+            if (id.includes('lodash')) {
+              return 'lodash-vendors';
+            }
+            if (id.includes('bootstrap')) {
+              return 'bootstrap-vendors';
+            }
+          }
         },
-        injectRegister: 'auto',
-        includeAssets: ['vite.svg', 'robots.txt', 'apple-touch-icon.png'],
-        manifest: {
-          name: 'React Template',
-          short_name: 'App',
-          description: 'React Template with Vite',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'pwa-64x64.png',
-              sizes: '64x64',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: 'maskable-icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable',
-            },
-          ],
-        },
-      }),
-    ],
-    css: {
-      postcss: {
-        plugins: [tailwindcss()],
       },
     },
-  };
+  },
 });
